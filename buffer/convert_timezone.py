c72641da8904f17c5a1870deb8c48c81e3b171ac
Yiyang Tsui <yytsui@gmail.com>
2013-06-04 11:30:51 -0700

    add time parsing test

--- buffer/convert_timezone.py
+++ buffer/convert_timezone.py
@@ -26,13 +26,25 @@ def test_now():
     dt_now = datetime.now() #naive datetime object
     dt_from_aware, dt_to_aware = convert_timezone('Canada/Pacific', 'Canada/Eastern',dt_now)
     timediff = dt_to_aware - dt_from_aware
-
     assert timediff.total_seconds() == 0
     assert dt_to_aware.hour - dt_from_aware.hour == 3
     print dt_from_aware.strftime(TIME_FORMAT)
     print dt_to_aware.strftime(TIME_FORMAT)
 
+def test_parse_time():
+    """
+    test parsing from string:2013-05-23T15:36:27
+    """
+    timestr = "2013-05-23T15:36:27"
+    dt = datetime.strptime(timestr,"%Y-%m-%dT%H:%M:%S")
+    dt_from_aware, dt_to_aware = convert_timezone('Canada/Mountain', 'Canada/Eastern',dt)
+    timediff = dt_to_aware - dt_from_aware
+    assert timediff.total_seconds() == 0
+    assert dt_to_aware.hour - dt_from_aware.hour == 2
+    print dt_from_aware.strftime(TIME_FORMAT)
+    print dt_to_aware.strftime(TIME_FORMAT)
 
 
 if __name__ == '__main__':
     test_now()
+    test_parse_time()
